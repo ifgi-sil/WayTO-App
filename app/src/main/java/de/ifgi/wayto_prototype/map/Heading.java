26ba4ddf7d2c5bdbd23636c8177078c5e91c13de
Marius Runde <marius.runde@uni-muenster.de>
2014-12-08 23:32:07 +0100

    Create Heading class

---
+++ app/src/main/java/de/ifgi/wayto_prototype/map/Heading.java
@@ -0,0 +1,42 @@
+package de.ifgi.wayto_prototype.map;
+
+/**
+ * Class to store information about heading directions
+ *
+ * @author Marius Runde
+ */
+public abstract class Heading {
+
+    /**
+     * North direction ending angle in degree
+     */
+    public static final double NORTH = 45;
+    /**
+     * East direction ending angle
+     */
+    public static final double EAST = 135;
+    /**
+     * South direction ending angle
+     */
+    public static final double SOUTH = -135;
+    /**
+     * West direction ending angle
+     */
+    public static final double WEST = -45;
+    /**
+     * ID of the North direction
+     */
+    public static final int NORTH_ID = 0;
+    /**
+     * ID of the East direction
+     */
+    public static final int EAST_ID = 1;
+    /**
+     * ID of the South direction
+     */
+    public static final int SOUTH_ID = 2;
+    /**
+     * ID of the West direction
+     */
+    public static final int WEST_ID = 3;
+}
