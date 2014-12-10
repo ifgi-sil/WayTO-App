323e114dddd8692d0592eb09a3301b72ea124310
Marius Runde <marius.runde@uni-muenster.de>
2014-12-10 10:55:41 +0100

    Add setting to disable off-screen landmarks

--- app/src/main/java/de/ifgi/wayto_prototype/activities/MainActivity.java
+++ app/src/main/java/de/ifgi/wayto_prototype/activities/MainActivity.java
@@ -43,6 +43,7 @@ import java.util.Collections;
 import java.util.Locale;
 
 import de.ifgi.wayto_prototype.R;
+import de.ifgi.wayto_prototype.demo.LandmarkCollection;
 import de.ifgi.wayto_prototype.landmarks.Landmark;
 import de.ifgi.wayto_prototype.landmarks.PointLandmark;
 import de.ifgi.wayto_prototype.landmarks.RegionalLandmark;
@@ -122,14 +123,18 @@ public class MainActivity extends FragmentActivity implements GoogleMap.OnCamera
      */
     private int prefMapType;
     /**
+     * Preference value indicating whether off-screen landmarks shall be displayed
+     */
+    private boolean prefMethod;
+    /**
      * Preference value for the method for displaying off-screen landmarks (e.g. wedge)
      */
-    private int prefMethod;
+    private int prefMethodType;
 
     /**
      * List of landmarks
      */
-    private final ArrayList<Landmark> ALL_LANDMARKS = new ArrayList<Landmark>();
+    private final ArrayList<Landmark> ALL_LANDMARKS = LandmarkCollection.initLandmarks();
     /**
      * Google Maps object
      */
@@ -157,9 +162,6 @@ public class MainActivity extends FragmentActivity implements GoogleMap.OnCamera
 
         // Setup the map
         setupMap();
-
-        // Initialise the landmarks
-        initLandmarks();
     }
 
     @Override
@@ -199,7 +201,9 @@ public class MainActivity extends FragmentActivity implements GoogleMap.OnCamera
         prefMapGoogle = preferences.getBoolean(SettingsActivity.PREF_KEY_MAP_GOOGLE, true);
         prefMapType = Integer.valueOf(
                 preferences.getString(SettingsActivity.PREF_KEY_MAP_TYPE, getString(R.string.map_type_normal)));
-        prefMethod = Integer.valueOf(preferences.getString(SettingsActivity.PREF_KEY_METHOD, "2"));
+        prefMethod = preferences.getBoolean(SettingsActivity.PREF_KEY_METHOD, true);
+        prefMethodType = Integer.valueOf(
+                preferences.getString(SettingsActivity.PREF_KEY_METHOD_TYPE, "2"));
     }
 
     /**
@@ -218,63 +222,17 @@ public class MainActivity extends FragmentActivity implements GoogleMap.OnCamera
             prefMapType = mapType;
             updateMapType();
         }
-        int method = Integer.valueOf(preferences.getString(SettingsActivity.PREF_KEY_METHOD, "2"));
+        boolean method = preferences.getBoolean(SettingsActivity.PREF_KEY_METHOD, true);
         if (prefMethod != method) {
             prefMethod = method;
             updateMap();
         }
-    }
-
-    /**
-     * Initialise the landmarks in order of the reference radius
-     */
-    private void initLandmarks() {
-        /*ALL_LANDMARKS.add(new RegionalLandmark("Aasee", 1500, R.drawable.landmark_water_body,
-                new LatLng[]{
-                        new LatLng(51.95728, 7.61329), new LatLng(51.95688, 7.61368),
-                        new LatLng(51.95656, 7.61286), new LatLng(51.95675, 7.6126),
-                        new LatLng(51.95596, 7.6111), new LatLng(51.95471, 7.60973),
-                        new LatLng(51.95395, 7.60835), new LatLng(51.95339, 7.60638),
-                        new LatLng(51.95265, 7.60655), new LatLng(51.95106, 7.60509),
-                        new LatLng(51.95064, 7.60471), new LatLng(51.94874, 7.60054),
-                        new LatLng(51.94744, 7.59904), new LatLng(51.94683, 7.59681),
-                        new LatLng(51.94651, 7.59578), new LatLng(51.94617, 7.59681),
-                        new LatLng(51.94516, 7.59702), new LatLng(51.94419, 7.59604),
-                        new LatLng(51.94318, 7.59458), new LatLng(51.94244, 7.59501),
-                        new LatLng(51.94278, 7.59668), new LatLng(51.94405, 7.59904),
-                        new LatLng(51.94625, 7.6005), new LatLng(51.94858, 7.60333),
-                        new LatLng(51.94934, 7.60462), new LatLng(51.94998, 7.60569),
-                        new LatLng(51.95045, 7.60608), new LatLng(51.95384, 7.61097),
-                        new LatLng(51.95458, 7.61376), new LatLng(51.95503, 7.61509),
-                        new LatLng(51.95635, 7.61771), new LatLng(51.95712, 7.61754),
-                        new LatLng(51.95754, 7.61406)
-                })); TODO */
-        ALL_LANDMARKS.add(new PointLandmark("Innenstadt", 2500, R.drawable.landmark_shopping,
-                new LatLng(51.957173, 7.627344)));
-        ALL_LANDMARKS.add(new PointLandmark("Schloss", 2750, R.drawable.landmark_castle,
-                new LatLng(51.963622, 7.613187)));
-        ALL_LANDMARKS.add(new PointLandmark("UKM", 2250, R.drawable.landmark_hospital,
-                new LatLng(51.9603, 7.596262)));
-        ALL_LANDMARKS.add(new PointLandmark("Halle Münsterland", 1750, R.drawable.landmark_stadium,
-                new LatLng(51.949066, 7.63982)));
-        ALL_LANDMARKS.add(new PointLandmark("Leonardo Campus", 1700, R.drawable.landmark_university,
-                new LatLng(51.973388, 7.601604)));
-        ALL_LANDMARKS.add(new PointLandmark("York Center", 1650, R.drawable.landmark_shopping,
-                new LatLng(51.973418, 7.611111)));
-        ALL_LANDMARKS.add(new PointLandmark("Hbf", 1500, R.drawable.landmark_train_station,
-                new LatLng(51.956667, 7.635)));
-        ALL_LANDMARKS.add(new PointLandmark("Aasee", 1500, R.drawable.landmark_water_body,
-                new LatLng(51.949444, 7.603514)));
-        ALL_LANDMARKS.add(new PointLandmark("Dom", 1450, R.drawable.landmark_church,
-                new LatLng(51.962825, 7.625772)));
-        ALL_LANDMARKS.add(new PointLandmark("Preußenstadion", 1300, R.drawable.landmark_stadium,
-                new LatLng(51.92917, 7.624766)));
-        ALL_LANDMARKS.add(new PointLandmark("Buddenturm", 720, R.drawable.landmark_tower,
-                new LatLng(51.96623, 7.623138)));
-        ALL_LANDMARKS.add(new PointLandmark("Aral Tankstelle", 550, R.drawable.landmark_gas_station,
-                new LatLng(51.967284, 7.61386)));
-        ALL_LANDMARKS.add(new PointLandmark("Picasso Museum", 500, R.drawable.landmark_museum,
-                new LatLng(51.959883, 7.62651)));
+        int methodType = Integer.valueOf(
+                preferences.getString(SettingsActivity.PREF_KEY_METHOD_TYPE, "2"));
+        if (prefMethodType != methodType) {
+            prefMethodType = methodType;
+            updateMap();
+        }
     }
 
     /**
@@ -499,9 +457,10 @@ public class MainActivity extends FragmentActivity implements GoogleMap.OnCamera
             );
             Log.d(TAG, getString(R.string.log_map_point_landmark_added));
 
-            if (distance != MARKER_ON_SCREEN) {
+            // Check if the landmark is an off-screen landmark and shall be displayed
+            if (distance != MARKER_ON_SCREEN && prefMethod) {
                 // Use preferred method for off-screen methods
-                switch (prefMethod) {
+                switch (prefMethodType) {
                     case METHOD_ARROW:
                         // Use the "normal arrow" method
                         addArrowToMap(landmark, distance);
